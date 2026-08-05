eb76225e947b33040a7fc5881f6cda426cf3bd89
AndradaAkki <andrada.stanculescu@stud.ubbcluj.ro>
2026-08-05 14:20:08 +0300

    feat: add TypeScript client generation from OpenAPI
    Refs: #58

--- frontend/src/app/app.config.ts
+++ frontend/src/app/app.config.ts
@@ -1,13 +1,18 @@
-import { ApplicationConfig, provideBrowserGlobalErrorListeners } from '@angular/core';
+import { ApplicationConfig, provideBrowserGlobalErrorListeners, importProvidersFrom } from '@angular/core';
 import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
 import {providePrimeNG} from 'primeng/config';
 
 import Aura from '@primeuix/themes/aura';
-
+import {ApiModule, Configuration} from '../../typescript-client';
 export const appConfig: ApplicationConfig = {
   providers: [
+    importProvidersFrom(
+      ApiModule.forRoot(() => new Configuration({
+        basePath: ''
+      }))
+    ),
     provideBrowserGlobalErrorListeners(),
     provideRouter(routes),
     providePrimeNG({
