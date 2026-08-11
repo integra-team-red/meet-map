e4d24bec5c891bf0609a3b05b7e203fe67a1c230
AndradaAkki <andrada.stanculescu@stud.ubbcluj.ro>
2026-08-11 17:13:09 +0300

    feat: display event list on the feed page and add mobile specific navbar
    Refs: #85

--- frontend/src/app/home/feature/home-page/home-page.ts
+++ frontend/src/app/home/feature/home-page/home-page.ts
@@ -1,8 +1,36 @@
-import { Component } from '@angular/core';
+import {Component, inject, OnInit, signal} from '@angular/core';
+import {EventDto} from '@app/api/model/eventDto';
+import {EventControllerService} from '@app/api/api/eventController.service';
+import {PageEventDto} from '@app/api/model/pageEventDto';
+import {FormsModule} from '@angular/forms';
+import {Router} from '@angular/router';
+import {EventCard} from '../../../shared/ui/event-card/event-card';
+
 
 @Component({
   selector: 'app-home-page',
-  imports: [],
+  imports: [
+    EventCard,
+    FormsModule,
+  ],
   templateUrl: './home-page.html',
 })
-export class HomePage {}
+export class HomePage implements OnInit {
+  eventService: EventControllerService = inject(EventControllerService);
+  router = inject(Router);
+
+  events = signal<EventDto[]>([])
+
+  ngOnInit(): void {
+    this.eventService.getAllEvents({
+      page: 0,
+      size: 20,
+    }).subscribe((response: PageEventDto) => {
+      this.events.set(response.content!)
+    })
+  }
+
+  openEvent(eventId: number): void {
+    this.router.navigate([`/events/${eventId}`])
+  }
+}
