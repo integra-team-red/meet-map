eaedbe928173e543b7f6e289a8a0a80cd44e6787
ianis <muresanianis450@gmail.com>
2026-08-19 16:12:52 +0300

    feat: HTTP Interceptor For Authentication Header
    Refs: #59

--- frontend/src/app/core/auth/auth-service.ts
+++ frontend/src/app/core/auth/auth-service.ts
@@ -1,9 +1,29 @@
-import { Injectable } from '@angular/core';
+import {Injectable} from '@angular/core';
 
 @Injectable({providedIn: 'root'})
 export class AuthService {
+  private readonly tokenKey = 'token';
+
+  setToken(token: string, remember = true): void {
+    this.clearToken();
+    if (remember) {
+      localStorage.setItem('token', token);
+    } else {
+      sessionStorage.setItem('token', token);
+    }
+  }
+
   getToken(): string | null {
     return localStorage.getItem('token') ??
       sessionStorage.getItem('token');
+  }
+
+  clearToken(): void {
+    localStorage.removeItem(this.tokenKey);
+    sessionStorage.removeItem(this.tokenKey);
+  }
+
+  isLoggedIn(): boolean {
+    return this.getToken() !== null;
   }
 }
--- frontend/src/app/core/interceptors/auth-interceptor.ts
+++ frontend/src/app/core/interceptors/auth-interceptor.ts
@@ -4,20 +4,28 @@ import {AuthService} from '../auth/auth-service';
 import {catchError, throwError} from 'rxjs';
 import {Router} from '@angular/router';
 
+
+const EXCLUDED_PATHS = ['/api/auth/login', '/api/auth/register'];
+
+
 export const authInterceptor: HttpInterceptorFn = (req, next) => {
-  const token = inject(AuthService).getToken();
+  if (EXCLUDED_PATHS.some(path => req.url.includes(path))) {
+    return next(req);
+  }
+
+  const authService = inject(AuthService);
+  const token = authService.getToken();
   const router = inject(Router);
 
   if (!token) {
-    if (req.url.includes("login")|| req.url.includes("register"))
       return next(req);
-    router.navigate(['/login']);
   }
 
   return next(req.clone({
     setHeaders: {Authorization: `Bearer ${token}`},
   })).pipe(catchError((error: HttpErrorResponse) => {
     if (error.status === 401 || error.status === 403) {
+      authService.clearToken();
       router.navigate(['/login']);
     }
     return throwError(() => error);
