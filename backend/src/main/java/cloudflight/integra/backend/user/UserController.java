24e9898602378b4cd6471648e7ada6ef5b884b61
gemcu <68012400+gemcu@users.noreply.github.com>
2026-08-21 18:24:20 +0300

    feat: display joined events on the profile page
    Refs: #99

--- backend/src/main/java/cloudflight/integra/backend/user/UserController.java
+++ backend/src/main/java/cloudflight/integra/backend/user/UserController.java
@@ -1,12 +1,24 @@
 package cloudflight.integra.backend.user;
 
+import cloudflight.integra.backend.event.EventMapper;
+import cloudflight.integra.backend.event.model.Event;
+import cloudflight.integra.backend.event.model.EventDto;
+import cloudflight.integra.backend.eventparticipation.EventParticipationService;
 import cloudflight.integra.backend.tag.TagMapper;
 import cloudflight.integra.backend.tag.model.TagDto;
 import cloudflight.integra.backend.user.model.UpdateUserTagsDto;
 import cloudflight.integra.backend.user.model.UserDto;
 import jakarta.validation.Valid;
+import io.swagger.v3.oas.annotations.Operation;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.web.PageableDefault;
 import org.springframework.http.MediaType;
 import org.springframework.security.core.Authentication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.HashSet;
@@ -20,9 +32,20 @@ public class UserController {
     private final UserMapper mapper;
     private final TagMapper tagMapper;
 
-    public UserController(UserService service, UserMapper mapper, TagMapper tagMapper) {
+    private final EventParticipationService epService;
+    private final EventMapper eventMapper;
+
+    public UserController(
+        UserService service,
+        UserMapper mapper,
+        EventParticipationService epService,
+        EventMapper eventMapper,
+        TagMapper tagMapper
+    ) {
         this.service = service;
         this.mapper = mapper;
+        this.epService = epService;
+        this.eventMapper = eventMapper;
         this.tagMapper = tagMapper;
     }
 
@@ -51,5 +74,13 @@ public class UserController {
             .stream()
             .map(tagMapper::toDto)
             .toList();
+    }
+
+    @GetMapping(value = "/{userId}/joined", produces = MediaType.APPLICATION_JSON_VALUE)
+    public Page<EventDto> getJoinedEvents(
+        @PageableDefault(size = 20, sort = "dateTime") Pageable pageable,
+        @PathVariable Long userId
+    ){
+        return epService.getEventsByParticipant(userId, pageable).map(eventMapper::toDto);
     }
 }
