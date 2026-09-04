a0494b9db2afbc933a42f46f16801cd12452d8a9
gemcu <68012400+gemcu@users.noreply.github.com>
2026-09-04 17:34:22 +0300

    chore: remove coffee and coffemug packages
    Refs: #190

--- backend/src/main/java/cloudflight/integra/backend/coffeemug/model/CoffeeMugDto.java
+++
@@ -1,6 +0,0 @@
-package cloudflight.integra.backend.coffeemug.model;
-
-import cloudflight.integra.backend.coffee.model.CoffeeDto;
-
-public record CoffeeMugDto(Long id, String color, int capacityMl, boolean clean, CoffeeDto coffee) {
-}
